9a1cc8ad592b98c2f5f6940a7581a82d3d2f8502
Henrique Chaves <44180294+henchaves@users.noreply.github.com>
2025-06-12 14:34:22 +0200

    fix: checks type in helper functions (#55)
    * update checks in ConversationsResource
    
    * add tests for conversations resource
    
    * fix: checks type:
    
    * make type hint compatible with python 3.9
    
    * make type hint compatible with python 3.9
    
    * add post init
    
    * fix CheckConfig

---
+++ src/giskard_hub/data/check.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Union
+
+from ._base import BaseData
+
+
+# Backend DTO
+@dataclass
+class TestCaseCheckConfig(BaseData):
+    identifier: str
+    assertions: List[Dict[str, Any]]
+    enabled: bool
+
+
+# SDK DTO
+@dataclass
+class CheckConfig(BaseData):
+    identifier: str
+    params: Optional[Dict[str, Any]] = None
+    enabled: bool = True
+
+
+def _format_checks_to_cli(
+    checks: List[Union[TestCaseCheckConfig, Dict[str, Any]]],
+) -> List[CheckConfig]:
+    if not checks:
+        return []
+
+    checks = [check if isinstance(check, dict) else check.to_dict() for check in checks]
+
+    return [
+        CheckConfig.from_dict(
+            {
+                "identifier": check["identifier"],
+                "enabled": check["enabled"],
+                **(
+                    {"params": params}
+                    if check.get("assertions")
+                    and (
+                        params := {
+                            k: v
+                            for k, v in check["assertions"][0].items()
+                            if k != "type"
+                        }
+                    )
+                    else {}
+                ),
+            }
+        )
+        for check in checks
+    ]
+
+
+def _format_checks_to_backend(
+    checks: List[Union[CheckConfig, Dict[str, Any]]],
+) -> List[TestCaseCheckConfig]:
+    if not checks:
+        return []
+
+    checks = [check if isinstance(check, dict) else check.to_dict() for check in checks]
+
+    return [
+        TestCaseCheckConfig.from_dict(
+            {
+                "enabled": True,  # Default value for enabled
+                **check,
+                **(
+                    {"assertions": [{"type": check["identifier"], **check["params"]}]}
+                    if check.get("params")
+                    else {}
+                ),
+            }
+        )
+        for check in checks
+    ]
