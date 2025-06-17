f0498d81bbd353f9e08bfb0ec8343f79c7b98dc1
Henrique Chaves <henrique@giskard.ai>
2025-06-17 11:34:38 +0200

    add ChecksResource.update

--- src/giskard_hub/resources/checks.py
+++ src/giskard_hub/resources/checks.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, List, Optional
 
+from ..data._base import NOT_GIVEN, filter_not_given
 from ..data.check import Check, extract_check_params
 from ._resource import APIResource
 
@@ -52,4 +53,26 @@ class ChecksResource(APIResource):
                 "assertions": [params],
             },
         )
+        return Check.from_dict({**data, "params": extract_check_params(data)})
+
+    def update(
+        self,
+        check_id: str,
+        *,
+        identifier: Optional[str] = NOT_GIVEN,
+        name: Optional[str] = NOT_GIVEN,
+        description: Optional[str] = NOT_GIVEN,
+        params: Optional[Dict[str, Any]] = NOT_GIVEN,
+    ) -> Check:
+
+        data = filter_not_given(
+            {
+                "identifier": identifier,
+                "name": name,
+                "description": description,
+                "assertions": [params] if params != NOT_GIVEN else NOT_GIVEN,
+            }
+        )
+
+        data = self._client.patch(f"/checks/{check_id}", json=data)
         return Check.from_dict({**data, "params": extract_check_params(data)})
