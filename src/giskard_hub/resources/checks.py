85a79dcd29edeb68e5e5dd39b20848f760216f01
Henrique Chaves <henrique@giskard.ai>
2025-06-17 11:18:12 +0200

    add ChecksResource.delete

--- src/giskard_hub/resources/checks.py
+++ src/giskard_hub/resources/checks.py
@@ -1,10 +1,15 @@
+from typing import List
+
 from ..data.check import Check, extract_check_params
 from ._resource import APIResource
 
 
 class ChecksResource(APIResource):
     def list(self, project_id: str):
-        data = self._client.get(f"/checks?project_id={project_id}&filter_builtin=true")
+        data = self._client.get(
+            "/checks",
+            params={"project_id": project_id, "filter_builtin": True},
+        )
 
         return [
             Check.from_dict(
@@ -24,3 +29,6 @@ class ChecksResource(APIResource):
                 "params": extract_check_params(data),
             }
         )
+
+    def delete(self, check_id: str | List[str]) -> None:
+        return self._client.delete("/checks", params={"check_ids": check_id})
