f77af6375dd330ae7527a4003762e4b1bb3d881f
Henrique Chaves <henrique@giskard.ai>
2025-06-17 10:48:36 +0200

    add ChecksResource.list

---
+++ src/giskard_hub/resources/checks.py
@@ -0,0 +1,19 @@
+from ..data.check import Check, extract_check_params
+from ._resource import APIResource
+
+
+class ChecksResource(APIResource):
+    def list(self, project_id: str):
+        data = self._client.get(f"/checks?project_id={project_id}&filter_builtin=true")
+
+        checks = [
+            Check.from_dict(
+                {
+                    **check,
+                    **extract_check_params(check),
+                }
+            )
+            for check in data
+        ]
+
+        return checks
