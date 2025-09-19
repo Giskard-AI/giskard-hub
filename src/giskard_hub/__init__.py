910d27f314586b56726638971407fb8ebcd2687f
Hartorn <hartorn.github@gmail.com>
2025-09-19 12:30:43 +0200

    format

--- src/giskard_hub/__init__.py
+++ src/giskard_hub/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 from .client import HubClient
-from .data import *
 
 # Import data module's __all__ to avoid duplication
+from .data import *
 from .data import __all__ as _data_all
 
 __all__ = ["HubClient"] + _data_all
