7d12c989146959f8341d1852fb70959bee7951db
Kevin Messiaen <kevin.messiaen@icloud.com>
2025-12-17 17:05:59 +0700

    chore: removed hub sdk

--- src/giskard_hub/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .client import HubClient
-
-# Import data module's __all__ to avoid duplication
-from .data import *
-from .data import __all__ as _data_all
-
-__all__ = ["HubClient"] + _data_all
